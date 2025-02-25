af5d33d2f318fe84b731493fc7fed780c27fcd83
QueGem <woals11298@gmail.com>
2025-02-25 10:17:10 +0900

    레벨 1 코드개선

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    @NotBlank
+    @NotBlank(message = "현재 비밀번호는 필수값입니다.")
     private String oldPassword;
-    @NotBlank
+
+    @NotBlank(message = "새 비밀번호는 필수값입니다")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야합니다.")
+    @Pattern(regexp = ".*\\d.*", message = "비밀번호에는 숫자가 1개 이상 포함되어야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "비밀번호에는 대문자가 1개 이상 포함되어야합니다.")
     private String newPassword;
 }
